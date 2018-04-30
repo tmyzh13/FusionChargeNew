1f9e28a92c5584d4f46c5fee7936312eb0ed6d28
chenyonglin1029@163.com <123456789c>
2018-04-30 09:02:42 +0800

    修改搜索的bug

---
+++ app/src/main/java/com/huawei/fusionchargeapp/views/interfaces/UserInfoView.java
@@ -0,0 +1,11 @@
+package com.huawei.fusionchargeapp.views.interfaces;
+
+import com.corelibs.base.BaseView;
+
+/**
+ * Created by issuser on 2018/4/19.
+ */
+
+public interface UserInfoView extends BaseView{
+  
+}
