8b1c391ca56172dd029b123cce717ddaf19e013e
yzh <356589990@qq.com>
2018-05-18 18:14:11 +0800

    yzh

---
+++ app/src/main/java/com/huawei/fusionchargeapp/presenter/MyAcountPresenter.java
@@ -0,0 +1,15 @@
+package com.huawei.fusionchargeapp.presenter;
+
+import com.corelibs.base.BasePresenter;
+import com.huawei.fusionchargeapp.views.interfaces.MyAcountView;
+
+/**
+ * Created by issuser on 2018/5/18.
+ */
+
+public class MyAcountPresenter extends BasePresenter<MyAcountView> {
+    @Override
+    public void onStart() {
+
+    }
+}
