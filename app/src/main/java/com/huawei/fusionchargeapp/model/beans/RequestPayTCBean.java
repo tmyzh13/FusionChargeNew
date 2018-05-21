9a8670ef0fe713126bf96bc7fce3ea8130079d8b
yzh <356589990@qq.com>
2018-05-21 18:32:03 +0800

    lt

---
+++ app/src/main/java/com/huawei/fusionchargeapp/model/beans/RequestPayTCBean.java
@@ -0,0 +1,12 @@
+package com.huawei.fusionchargeapp.model.beans;
+
+/**
+ * Created by issuser on 2018/5/21.
+ */
+
+public class RequestPayTCBean {
+    public String orderRecordNum;
+    public double totalFee;
+    public int payType;
+    public long businessPackageId;
+}
