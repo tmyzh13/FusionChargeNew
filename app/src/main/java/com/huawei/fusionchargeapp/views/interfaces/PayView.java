612039764e21193138b072f5cb5d0f19ee7de5b5
yzh <356589990@qq.com>
2018-05-21 18:49:03 +0800

    lt

--- app/src/main/java/com/huawei/fusionchargeapp/views/interfaces/PayView.java
+++ app/src/main/java/com/huawei/fusionchargeapp/views/interfaces/PayView.java
@@ -1,16 +1,20 @@
 package com.huawei.fusionchargeapp.views.interfaces;
 
 import com.corelibs.base.BaseView;
+import com.huawei.fusionchargeapp.model.beans.MyTaocanBean;
 import com.huawei.fusionchargeapp.model.beans.PayInfoBean;
+import com.huawei.fusionchargeapp.model.beans.TaocanBean;
+
+import java.util.List;
 
 /**
  * Created by issuser on 2018/4/25.
  */
 
 public interface PayView  extends BaseView{
-
     void renderData(PayInfoBean bean);
     void paySuccess();
     void payBalanceNotEnough();
     void payFail();
+    void renderMyTaoCan(MyTaocanBean bean);
 }
